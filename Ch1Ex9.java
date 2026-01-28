c34f6e79aa19821b6f4f67b7dc3aaa75084dab58
antor1112225 <tahmidantor123@gmail.com>
2026-01-28 11:48:19 +0600

    Create Ch1Ex9.java

---
+++ Ch1Ex9.java
@@ -0,0 +1,16 @@
+public class Ch1Ex9 {
+
+
+
+    public static void main(String[] args) {
+
+        float width = 4.5f;
+        float height = 7.9f;
+
+        float area = width * height;
+        float perimeter = 2 * (width + height);
+
+        System.out.println("Area of the rectangle is: " + area);
+        System.out.println("Perimeter of the rectangle is: " + perimeter);
+    }
+}
